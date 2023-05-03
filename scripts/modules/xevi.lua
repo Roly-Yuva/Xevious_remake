401c7310c0069451dc9b463fe47bc10694661404
Roly-Yuva <codedyuva333@gmail.com>
2023-05-03 18:52:43 -0400

    ai changes, with module for useful functions

---
+++ scripts/modules/xevi.lua
@@ -0,0 +1,23 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+M.check_proximity = function(object_pos, point, distance)
+	local x_check = true
+	local y _check = true
+	if object_pos.x >= point.x + distance or object_pos.x <= point.x - distance then
+		x_check = false
+	else
+		return true
+	end
+	if object_pos.y >= point.y + distance or object_pos.y <= point.y - distance then
+		y_check = false
+	else
+		return true
+	end
+	return x_check and y_check
+end
+
+return M
